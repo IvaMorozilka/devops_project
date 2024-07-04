d3d554a37facb50cba3f69f40e4bccba893e2d11
IvaMorozilka <ivanmorozko97@gmail.com>
2024-07-04 22:15:57 +0300

    feat(modes): User and Todo entities are described

---
+++ services/backend/src/database/models.py
@@ -0,0 +1,18 @@
+from tortoise import models, fields
+
+
+class User(models.Model):
+    id = fields.IntField(primary_key=True)
+    username = fields.CharField(max_length=50, null=True)
+    login = fields.CharField(max_length=32, unique = True)
+    password = fields.CharField(max_length=128, null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+
+class Todo(models.Model):
+    id = fields.IntField(pk=True)
+    content = fields.TextField()
+    author = fields.ForeignKeyField("models.Users", related_name="note")
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+    complete = fields.BooleanField()
