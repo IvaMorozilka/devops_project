07a1261758c10afaa39744ea7430e7fbc5e46a2f
IvaMorozilka <ivanmorozko97@gmail.com>
2024-07-06 16:31:53 +0300

    feat(crud): Added helper functions for CRUD

---
+++ services/backend/src/crud/todos.py
@@ -0,0 +1,55 @@
+from fastapi import HTTPException
+from tortoise.exceptions import DoesNotExist
+
+# Fix needed for import. Will be removed in docker.
+from inspect import getsourcefile
+import os.path as path, sys
+current_dir = path.dirname(path.abspath(getsourcefile(lambda:0)))
+sys.path.insert(0, current_dir[:current_dir.rfind(path.sep)])
+# 
+
+from database.models import Todos
+from schemas.todos import TodoOutSchema
+
+
+async def get_Todos():
+    return await TodoOutSchema.from_queryset(Todos.all())
+
+
+async def get_note(todo_id) -> TodoOutSchema:
+    return await TodoOutSchema.from_queryset_single(Todos.get(id=todo_id))
+
+
+async def create_todo(todo, current_user) -> TodoOutSchema:
+    todo_dict = todo.dict(exclude_unset=True)
+    todo_dict["author_id"] = current_user.id
+    todo_obj = await Todos.create(**todo_dict)
+    return await TodoOutSchema.from_tortoise_orm(todo_obj)
+
+
+async def update_todo(todo_id, todo, current_user) -> TodoOutSchema:
+    try:
+        db_todo = await TodoOutSchema.from_queryset_single(Todos.get(id=todo_id))
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail=f"Todo {todo_id} does not exist")
+
+    if db_todo.author.id == current_user.id:
+        await Todos.filter(id=todo_id).update(**todo.dict(exclude_unset=True))
+        return await TodoOutSchema.from_queryset_single(Todos.get(id=todo_id))
+
+    raise HTTPException(status_code=403, detail=f"Not authorized to update")
+
+
+async def delete_note(todo_id, current_user):
+    try:
+        db_todo = await TodoOutSchema.from_queryset_single(Todos.get(id=todo_id))
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail=f"Todo {todo_id} does not exist")
+
+    if db_todo.author.id == current_user.id:
+        deleted_count = await Todos.filter(id=todo_id).delete()
+        if not deleted_count:
+            raise HTTPException(status_code=404, detail=f"Todo {todo_id} does not exist")
+        return f"Deleted note {todo_id}"
+
+    raise HTTPException(status_code=403, detail=f"Not authorized to delete")
---
+++ services/backend/src/crud/users.py
@@ -0,0 +1,40 @@
+from fastapi import HTTPException
+from passlib.context import CryptContext
+from tortoise.exceptions import DoesNotExist, IntegrityError
+
+# Fix needed for import. Will be removed in docker.
+from inspect import getsourcefile
+import os.path as path, sys
+current_dir = path.dirname(path.abspath(getsourcefile(lambda:0)))
+sys.path.insert(0, current_dir[:current_dir.rfind(path.sep)])
+# 
+
+from database.models import Users
+from schemas.users import UserOutSchema
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+async def create_user(user) -> UserOutSchema:
+    user.password = pwd_context.encrypt(user.password)
+
+    try:
+        user_obj = await Users.create(**user.dict(exclude_unset=True))
+    except IntegrityError:
+        raise HTTPException(status_code=401, detail=f"Sorry, this username is already exists. Try a different one.")
+
+    return await UserOutSchema.from_tortoise_orm(user_obj)
+
+async def delete_user(user_id, current_user):
+    try:
+        db_user = await UserOutSchema.from_queryset_single(Users.get(id=user_id))
+    except DoesNotExist:
+        raise HTTPException(status_code=404, detail=f"User {user_id} does not exist.")
+
+    if db_user.id == current_user.id:
+        deleted_count = await Users.filter(id=user_id).delete()
+        if not deleted_count:
+            raise HTTPException(status_code=404, detail=f"User {user_id} does not exist.")
+        return f"Deleted user {user_id}"
+
+    raise HTTPException(status_code=403, detail=f"Not authorized to delete")
